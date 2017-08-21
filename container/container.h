00506f9856b346f72bc1ff0ee6f1fda663044e69
Johan Sannemo <jsannemo@jsannemo.se>
2017-08-21 19:27:24 +0200

    Fix Container to not be copyable

--- container/container.h
+++ container/container.h
@@ -14,20 +14,22 @@ namespace omogenexec {
 // Note that the constructor creates the new process ahead-of-time and performs some setup.
 // The remaining setup is performed once Execute() is called, minimizing the latency a bit.
 class Container {
+
     // Process ID of the child process we are executing the new program in (called init)
     pid_t initPid;
+    // Have the contained process been waited for?
+    bool waitedFor;
+
     // Since we may receive the execution request after starting the new process, we use
     // a pipe to send the request to the process.
     int commandPipe[2];
     // To be able to distinguish between e.g. the program itself exiting and setup failing,
     // we keep an additional pipe that the new process can use to send us errors.
     int errorPipe[2];
-    unique_ptr<Cgroup> cgroup;
     // The path to the new root with specific paths mounted to it
-	std::string containerRoot;
+    std::string containerRoot;
 
-    // Have the contained process been waited for?
-    bool waitedFor;
+    unique_ptr<Cgroup> cgroup;
 
     ExecuteResponse monitorInit(const ResourceLimits& limits);
     void killInit();
@@ -41,7 +43,7 @@ public:
     Container();
     ~Container();
 
-    Container(const Container&) = default;
+    Container(const Container&) = delete;
     Container& operator=(const Container&) = delete;
 };
 
