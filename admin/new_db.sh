4549576fea884c7060a3903846d36ff28a2b990d
Johan Sannemo <jsannemo@jsannemo.se>
2019-08-06 20:08:25 +0200

    Update deployment scripts

---
+++ admin/new_db.sh
@@ -0,0 +1,10 @@
+#!/usr/bin/env bash
+set -e
+
+dropdb omogenjudge --if-exists
+dropuser omogenjudge --if-exists
+psql -c "CREATE USER omogenjudge WITH PASSWORD 'omogenjudge';"
+createdb omogenjudge
+psql omogenjudge -f schema/schema.sql
+
+psql omogenjudge -c "INSERT INTO account(username, password_hash) VALUES('test', '\$2a\$10\$r8xXriU.jnygztki.9eCv.C91FgU4BXnK/4Kl087v8RWsfGW0wcwW');"
--- admin/reset_schema.sh
+++ admin/reset_schema.sh
@@ -12,13 +12,9 @@ esac; shift; done
 sudo service omogenjudge-local stop
 sudo service omogenjudge-master stop
 sudo service omogenjudge-frontend stop
-dropdb omogenjudge --if-exists
-dropuser omogenjudge --if-exists
-psql -c "CREATE USER omogenjudge WITH PASSWORD 'omogenjudge';"
-createdb omogenjudge
-psql omogenjudge -f schema/schema.sql
 
-psql omogenjudge -c "INSERT INTO account(username, password_hash) VALUES('test', '\$2a\$10\$r8xXriU.jnygztki.9eCv.C91FgU4BXnK/4Kl087v8RWsfGW0wcwW');"
+./admin/new_db.sh
+
 sudo service omogenjudge-local start
 sudo service omogenjudge-master start
 if [[ $nofrontend -ne 1 ]]; then
