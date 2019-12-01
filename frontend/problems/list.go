41dafba1a0b052e0ed45a175ec803cbf2d76c222
Johan Sannemo <jsannemo@google.com>
2019-12-02 00:30:17 +0100

    Disable the old problem list page.

--- frontend/problems/list.go
+++ frontend/problems/list.go
@@ -4,7 +4,6 @@ import (
 	"github.com/jsannemo/omogenjudge/frontend/paths"
 	"github.com/jsannemo/omogenjudge/frontend/request"
 	"github.com/jsannemo/omogenjudge/storage/models"
-	"github.com/jsannemo/omogenjudge/storage/problems"
 )
 
 type Params struct {
@@ -13,13 +12,5 @@ type Params struct {
 
 // ListHandler is the request handler for the problem list.
 func ListHandler(r *request.Request) (request.Response, error) {
-	if !r.Context.Contest.FullStart() {
-		return request.Redirect(paths.Route(paths.Home)), nil
-	}
-
-	probs, err := problems.List(r.Request.Context(), problems.ListArgs{WithStatements: problems.StmtTitles}, problems.ListFilter{})
-	if err != nil {
-		return nil, err
-	}
-	return request.Template("problems_list", &Params{probs}), nil
+	return request.Redirect(paths.Route(paths.Home)), nil
 }
