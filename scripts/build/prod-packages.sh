38902c0eb87f052ec95a4aa641a66d9c8e2abcfa
Fredrik Ekholm <fredrik.w.ekholm@gmail.com>
2019-12-01 18:33:02 +0100

    Make bash scripts invariant to directory of execution

--- scripts/build/prod-packages.sh
+++ scripts/build/prod-packages.sh
@@ -1,6 +1,9 @@
 #!/usr/bin/env bash
 set -e
 
+SCRIPT_DIR=$(dirname "$(readlink -f "$0")")
+cd $SCRIPT_DIR/../../
+
 bazel build -c opt {frontend,localjudge,masterjudge,sandbox}/deb/...
 mkdir -p builds
 
