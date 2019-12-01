b22013a6998d2eb77a7a0dd41b96fb078487f8de
Fredrik Ekholm <fredrik.w.ekholm@gmail.com>
2019-12-01 18:33:02 +0100

    Make bash scripts invariant to directory of execution

--- scripts/build/all.sh
+++ scripts/build/all.sh
@@ -1,2 +1,6 @@
 #!/usr/bin/env bash
+
+SCRIPT_DIR=$(dirname "$(readlink -f "$0")")
+cd $SCRIPT_DIR/../../
+
 bazel build {admin,filehandler,frontend,localjudge,masterjudge,problemtools,runner,sandbox,schema,storage,util,vendor}/...
--- scripts/deploy/local.sh
+++ scripts/deploy/local.sh
@@ -1,6 +1,9 @@
 #!/usr/bin/env bash
 set -e
 
+SCRIPT_DIR=$(dirname "$(readlink -f "$0")")
+cd $SCRIPT_DIR/../../
+
 scripts/build/packages.sh
 
 sudo dpkg -i builds/omogenjudge-local-dev.deb
