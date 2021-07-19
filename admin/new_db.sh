1ab5099ffd89c1a2435e20014cdd1f0d7db1c07a
Johan Sannemo <jsannemo@jsannemo.se>
2021-07-19 10:50:20 +0200

    Stop services that listen to the database before resetting it

--- admin/new_db.sh
+++ admin/new_db.sh
@@ -1,9 +1,15 @@
 #!/usr/bin/env bash
 set -e
 
+sudo service omogenjudge-queue stop
+sudo service omogenjudge-host stop
+
 dropdb omogenjudge --if-exists
 dropuser omogenjudge --if-exists
 dropuser omogenhost --if-exists
 psql -c "CREATE USER omogenjudge WITH PASSWORD 'omogenjudge';"
 psql -c "CREATE USER omogenhost WITH PASSWORD 'omogenhost';"
 createdb omogenjudge
+
+sudo service omogenjudge-host start
+sudo service omogenjudge-queue start
