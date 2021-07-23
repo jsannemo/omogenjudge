3e31d045848c14e028046ab1af2800ecc7e4ace0
Johan Sannemo <jsannemo@jsannemo.se>
2021-07-23 10:13:18 +0200

    Make DB reset script more robust

--- admin/new_db.sh
+++ admin/new_db.sh
@@ -1,15 +1,19 @@
 #!/usr/bin/env bash
 set -e
 
-sudo service omogenjudge-queue stop
-sudo service omogenjudge-host stop
+sudo service omogenjudge-queue stop || true
+sudo service omogenjudge-host stop || true
 
-dropdb omogenjudge --if-exists
-dropuser omogenjudge --if-exists
-dropuser omogenhost --if-exists
-psql -c "CREATE USER omogenjudge WITH PASSWORD 'omogenjudge';"
-psql -c "CREATE USER omogenhost WITH PASSWORD 'omogenhost';"
-createdb omogenjudge
+sudo -u postgres dropdb omogenjudge --if-exists
+sudo -u postgres dropuser omogenjudge --if-exists
+sudo -u postgres dropuser omogenhost --if-exists
+sudo -u postgres psql -c "CREATE USER omogenjudge WITH PASSWORD 'omogenjudge';"
+sudo -u postgres createdb omogenjudge
 
-sudo service omogenjudge-host start
-sudo service omogenjudge-queue start
+sudo service omogenjudge-host start || true
+sudo service omogenjudge-queue start || true
+
+echo "Database reset."
+echo "Run"
+echo "   > poetry run python manage.py migratedb"
+echo "to install the correct schema"
